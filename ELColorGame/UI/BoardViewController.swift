eb9397e3af622b7ffbc38aa542b011ae4089e6ff
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2015-09-24 14:48:15 +0200

    Implemented board view controller

--- ELColorGame/UI/BoardViewController.swift
+++ ELColorGame/UI/BoardViewController.swift
@@ -12,7 +12,17 @@ class BoardViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.redColor()
+        let grid_x = 6
+        let grid_y = 9
+        let dimensionSingleCell = 50
+        for var y = 0; y < grid_y; y++ {
+            for var x = 0; x < grid_x; x++ {
+                let ballView = CircleView(frame: CGRectMake(CGFloat(x * dimensionSingleCell + 5), CGFloat(y * dimensionSingleCell + 5),CGFloat(dimensionSingleCell), CGFloat(dimensionSingleCell)))
+                ballView.layer.cornerRadius = 50 / 2
+                self.view.addSubview(ballView)
+            }
+        }
+
     }
 }
 
