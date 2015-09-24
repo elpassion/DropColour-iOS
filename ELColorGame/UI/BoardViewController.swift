dafb07c9550feb7d8abb18a75e66eb0b11d82148
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2015-09-24 14:35:10 +0200

    Rename class to BoardViewController

---
+++ ELColorGame/UI/BoardViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BoardViewController.swift
+//  ELColorGame
+//
+//  Created by Mateusz Szklarek on 24/09/15.
+//  Copyright © 2015 EL Passion. All rights reserved.
+//
+
+import UIKit
+
+class BoardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.redColor()
+    }
+}
+
