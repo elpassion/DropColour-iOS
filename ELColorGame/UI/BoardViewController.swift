b02d8acd219227abf860fa19714a97142c5fb21c
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2015-09-24 23:31:55 +0200

    Modified views and adjustment to designs

--- ELColorGame/UI/BoardViewController.swift
+++ ELColorGame/UI/BoardViewController.swift
@@ -9,8 +9,9 @@
 import UIKit
 import SnapKit
 
-class BoardViewController: UIViewController {
+class BoardViewController: UIViewController, CircleViewPointChangeDelegate {
     
+    let diameterSingleCircle = 44
     var topView:UIView = UIView()
     var boardView:UIView = UIView()
     
@@ -18,29 +19,41 @@ class BoardViewController: UIViewController {
         super.viewDidLoad()
         
         configureTopView()
+        configureBackgroundBoardView()
         configureBoardView()
     }
     
     func configureTopView() {
-        topView.backgroundColor = UIColor.greenColor()
+        topView.backgroundColor = UIColor(red:0.22, green:0.2, blue:0.34, alpha:1)
         self.view.addSubview(topView)
         topView.snp_makeConstraints { (make) -> Void in
             make.top.equalTo(0)
             make.left.equalTo(0)
             make.right.equalTo(0)
-            make.height.equalTo(self.view).multipliedBy(0.15)
+            make.height.equalTo(self.view).multipliedBy(0.12)
         }
     }
     
     func configureBoardView() {
-        boardView.backgroundColor = UIColor.purpleColor()
-        let grid_x = 7
-        let grid_y = 10
-        let dimensionSingleCell = 50
+        boardView.backgroundColor = UIColor(red:0.22, green:0.2, blue:0.34, alpha:1)
+        let sizeXscreen:Int = Int(self.view.frame.size.width)
+        let sizeYscreen:Int = Int(self.view.frame.size.height) - Int(self.view.frame.size.height * 0.12)
+        let possibleCircleOnX = (sizeXscreen - 100 - (sizeXscreen % diameterSingleCircle)) / diameterSingleCircle
+        let possibleCircleOnY = (sizeYscreen - 150 - (sizeYscreen % diameterSingleCircle)) / diameterSingleCircle
+        print("sizeXscreen\(sizeXscreen)")
+        print("sizeYscreen\(sizeYscreen)")
+        let grid_x = possibleCircleOnX
+        let grid_y = possibleCircleOnY
+        print("prossibleViewOnScreenHorizontally: \(possibleCircleOnX)")
+        print("prossibleViewOnScreenVertically: \(possibleCircleOnY)")
+        let spacing = (self.view.frame.size.width - CGFloat(diameterSingleCircle * grid_x)) / CGFloat(grid_x + 1)
+        print("spacing\(spacing)")
+        var ballView: CircleView
         for var y = 0; y < grid_y; y++ {
             for var x = 0; x < grid_x; x++ {
-                let ballView = CircleView(frame: CGRectMake(CGFloat(x * dimensionSingleCell + 5), CGFloat(y * dimensionSingleCell + 5),CGFloat(dimensionSingleCell), CGFloat(dimensionSingleCell)))
-                ballView.layer.cornerRadius = 50 / 2
+                ballView = CircleView(frame: CGRectMake(CGFloat(x * (diameterSingleCircle + 10)) + spacing * 2, CGFloat(y * (diameterSingleCircle + 10)) + spacing * 2, CGFloat(diameterSingleCircle), CGFloat(diameterSingleCircle)))
+                ballView.delegate = self
+                ballView.layer.cornerRadius = CGFloat(diameterSingleCircle / 2)
                 self.boardView.addSubview(ballView)
             }
         }
@@ -51,6 +64,38 @@ class BoardViewController: UIViewController {
             make.right.equalTo(0)
             make.bottom.equalTo(0)
         }
+    }
+    
+    func configureBackgroundBoardView() {
+        let sizeXscreen:Int = Int(self.view.frame.size.width)
+        let sizeYscreen:Int = Int(self.view.frame.size.height) - Int(self.view.frame.size.height * 0.12)
+        let possibleCircleOnX = (sizeXscreen - 100 - (sizeXscreen % diameterSingleCircle)) / diameterSingleCircle
+        let possibleCircleOnY = (sizeYscreen - 150 - (sizeYscreen % diameterSingleCircle)) / diameterSingleCircle
+        let grid_x = possibleCircleOnX
+        let grid_y = possibleCircleOnY
+        let spacing = (self.view.frame.size.width - CGFloat(diameterSingleCircle * grid_x)) / CGFloat(grid_x + 1)
+        var ballView:UIView
+        for var y = 0; y < grid_y; y++ {
+            for var x = 0; x < grid_x; x++ {
+                ballView = UIView(frame: CGRectMake(CGFloat(x * (diameterSingleCircle + 10)) + spacing * 2, CGFloat(y * (diameterSingleCircle + 10)) + spacing * 2, CGFloat(diameterSingleCircle), CGFloat(diameterSingleCircle)))
+                ballView.backgroundColor = UIColor(red:0.24, green:0.23, blue:0.37, alpha:1)
+                ballView.layer.cornerRadius = CGFloat(diameterSingleCircle / 2)
+                self.boardView.addSubview(ballView)
+            }
+        }
+        self.view.addSubview(boardView)
+        boardView.snp_makeConstraints { (make) -> Void in
+            make.top.equalTo(self.topView.snp_bottom)
+            make.left.equalTo(0)
+            make.right.equalTo(0)
+            make.bottom.equalTo(0)
+        }
+    }
+    
+    //Delegate method 
+    
+    func pointDidChanged(point: CGPoint) {
+        print("point:\(point)")
     }
 }
 
--- ELColorGame/UI/CircleView.swift
+++ ELColorGame/UI/CircleView.swift
@@ -8,34 +8,55 @@
 
 import UIKit
 
+protocol CircleViewPointChangeDelegate {
+    func pointDidChanged(point:CGPoint)
+}
+
 class CircleView: UIView {
     
+    var delegate:CircleViewPointChangeDelegate? = nil
     var lastLocation:CGPoint = CGPointMake(0, 0)
-
+    var colors:[UIColor] = []
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         let touchRecognizer = UIPanGestureRecognizer(target: self, action: "detectTouch:")
         self.gestureRecognizers = [touchRecognizer]
-        
-        let blueValue = CGFloat(Int(arc4random() % 255)) / 255.0
-        let greenValue = CGFloat(Int(arc4random() % 255)) / 255.0
-        let redValue = CGFloat(Int(arc4random() % 255)) / 255.0
-        
-        self.backgroundColor = UIColor(red:redValue, green: greenValue, blue: blueValue, alpha: 1.0)
+        colors = [
+            UIColor(red:0.31, green:0.84, blue:0.84, alpha:1),
+            UIColor(red:0.34, green:0.85, blue:0.09, alpha:1),
+            UIColor(red:0.69, green:0.43, blue:0.84, alpha:1),
+            UIColor(red:0.91, green:0.14, blue:0.37, alpha:1),
+            UIColor(red:0.29, green:0.56, blue:0.89, alpha:1),
+        ]
+        self.backgroundColor = colors[Int(arc4random() % 4)]
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     func detectTouch(recognizer:UIPanGestureRecognizer) {
         let translation = recognizer.translationInView(self.superview)
         self.center = CGPointMake(lastLocation.x + translation.x, lastLocation.y + translation.y)
+        
+        print("center\(self.center)")
+        if (delegate != nil) {
+            let point:CGPoint = self.center
+            delegate?.pointDidChanged(point)
+        }
+        
+        if recognizer.state == .Began {
+            print("began")
+        } else if recognizer.state == .Changed {
+            print("changed")
+        } else if recognizer.state == .Ended {
+            print("ended")
+        }
     }
-    
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         self.superview?.bringSubviewToFront(self)
         lastLocation = self.center
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
 }
