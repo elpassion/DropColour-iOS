4c411bcd74d5825b11a9d5de041feb7388f2c91b
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2015-09-24 13:04:06 +0200

    Initial Commit

---
+++ ELColorGameTests/ELColorGameTests.swift
@@ -0,0 +1,36 @@
+//
+//  ELColorGameTests.swift
+//  ELColorGameTests
+//
+//  Created by Mateusz Szklarek on 24/09/15.
+//  Copyright © 2015 EL Passion. All rights reserved.
+//
+
+import XCTest
+@testable import ELColorGame
+
+class ELColorGameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
