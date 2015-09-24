e07ebe2cdf21cbd8fc6b442fbcf3016822282677
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2015-09-24 23:38:13 +0200

    Changed size of single circle

--- ELColorGame/UI/BoardViewController.swift
+++ ELColorGame/UI/BoardViewController.swift
@@ -11,7 +11,7 @@ import SnapKit
 
 class BoardViewController: UIViewController, CircleViewPointChangeDelegate {
     
-    let diameterSingleCircle = 44
+    let diameterSingleCircle = 50
     var topView:UIView = UIView()
     var boardView:UIView = UIView()
     
