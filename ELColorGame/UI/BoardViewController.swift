e37168f1062572319fc7c5831bff54b765632629
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2015-09-24 15:53:32 +0200

    Added topView to BoardViewController

--- ELColorGame/UI/BoardViewController.swift
+++ ELColorGame/UI/BoardViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import SnapKit
 
 class BoardViewController: UIViewController {
     
+    var topView:UIView = UIView()
+    var boardView:UIView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureBoard()
+        
+        configureTopView()
+        configureBoardView()
     }
-    func configureBoard() {
+    
+    func configureTopView() {
+        topView.backgroundColor = UIColor.greenColor()
+        self.view.addSubview(topView)
+        topView.snp_makeConstraints { (make) -> Void in
+            make.top.equalTo(0)
+            make.left.equalTo(0)
+            make.right.equalTo(0)
+            make.height.equalTo(self.view).multipliedBy(0.15)
+        }
+    }
+    
+    func configureBoardView() {
+        boardView.backgroundColor = UIColor.purpleColor()
         let grid_x = 7
         let grid_y = 10
         let dimensionSingleCell = 50
@@ -22,8 +41,15 @@ class BoardViewController: UIViewController {
             for var x = 0; x < grid_x; x++ {
                 let ballView = CircleView(frame: CGRectMake(CGFloat(x * dimensionSingleCell + 5), CGFloat(y * dimensionSingleCell + 5),CGFloat(dimensionSingleCell), CGFloat(dimensionSingleCell)))
                 ballView.layer.cornerRadius = 50 / 2
-                self.view.addSubview(ballView)
+                self.boardView.addSubview(ballView)
             }
+        }
+        self.view.addSubview(boardView)
+        boardView.snp_makeConstraints { (make) -> Void in
+            make.top.equalTo(self.topView.snp_bottom)
+            make.left.equalTo(0)
+            make.right.equalTo(0)
+            make.bottom.equalTo(0)
         }
     }
 }
