24270f44213d7b39838b60e54c45bae00850289e
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2015-09-25 17:39:57 +0200

    Added crashlytics framework

--- ELColorGame/Application/AppDelegate.swift
+++ ELColorGame/Application/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        Fabric.with([Crashlytics.self()])
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
         self.window?.rootViewController = StartViewController()
         self.window?.makeKeyAndVisible()
