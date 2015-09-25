ba70888855bff8b7ad0e2d27ef68732bdf0fdad3
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2015-09-25 12:24:37 +0200

    Added assets for icon app

---
+++ ELColorGame/UI/MenuViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MenuViewController.swift
+//  ELColorGame
+//
+//  Created by Mateusz Szklarek on 25/09/15.
+//  Copyright © 2015 EL Passion. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
