b01683c4280d6a22e6e838038553012d743eb195
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2015-09-27 16:42:24 +0200

    Refactoring extension 'UIView+CAAnimation' extracted code to private function

--- ELColorGame/UI/Extensions/UIView+CAAnimation/UIView+CAAnimation.swift
+++ ELColorGame/UI/Extensions/UIView+CAAnimation/UIView+CAAnimation.swift
@@ -11,31 +11,24 @@ import UIKit
 public extension UIView {
 
     func addIncreaseSizeAnimation() {
-        let keyFrame: CAKeyframeAnimation = CAKeyframeAnimation(keyPath: "transform.scale")
-        keyFrame.values = [1.2]
-        keyFrame.keyTimes = [1]
-        keyFrame.removedOnCompletion = false
-        keyFrame.fillMode = kCAFillModeForwards
-        keyFrame.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut), CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)]
-        self.layer.addAnimation(keyFrame, forKey: "transform.scale")
+        configureKeyFrameAnimationWithValues([1.2], keyTimes: [1])
     }
     
     func addBounceAnimation() {
-        let keyFrame: CAKeyframeAnimation = CAKeyframeAnimation(keyPath: "transform.scale")
-        keyFrame.values = [0.25, 0.4, 1.3, 1.0]
-        keyFrame.keyTimes = [0, 0.3, 0.75, 1]
-        keyFrame.duration = 0.3
-        keyFrame.removedOnCompletion = false
-        keyFrame.fillMode = kCAFillModeForwards
-        keyFrame.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut), CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)]
-        self.layer.addAnimation(keyFrame, forKey: "transform.scale")
+        configureKeyFrameAnimationWithValues([0.25, 0.4, 1.3, 1.0], keyTimes: [0, 0.3, 0.75, 1], duration: 0.3)
     }
     
     func addAppearAnimation() {
+        configureKeyFrameAnimationWithValues([0.0, 0.2, 0.5, 1.4, 1.0], keyTimes: [0, 0.3, 0.5, 0.75, 1], duration: 0.4)
+    }
+    
+    private func configureKeyFrameAnimationWithValues(values: [AnyObject], keyTimes: [NSNumber], duration: CFTimeInterval?=nil) {
         let keyFrame: CAKeyframeAnimation = CAKeyframeAnimation(keyPath: "transform.scale")
-        keyFrame.values = [0.0, 0.2, 0.5, 1.4, 1.0]
-        keyFrame.keyTimes = [0, 0.3, 0.5, 0.75, 1]
-        keyFrame.duration = 0.4
+        keyFrame.values = values
+        keyFrame.keyTimes = keyTimes
+        if let duration = duration {
+            keyFrame.duration = duration
+        }
         keyFrame.removedOnCompletion = false
         keyFrame.fillMode = kCAFillModeForwards
         keyFrame.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut), CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)]
