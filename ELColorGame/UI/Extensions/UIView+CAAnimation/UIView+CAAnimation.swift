46f8229c4a3547817242e69d99089a0b3d0c1ef2
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2015-09-27 16:02:44 +0200

    Created extension for UIView

---
+++ ELColorGame/UI/Extensions/UIView+CAAnimation/UIView+CAAnimation.swift
@@ -0,0 +1,13 @@
+//
+//  UIView+CAAnimation.swift
+//  ELColorGame
+//
+//  Created by Mateusz Szklarek on 27/09/15.
+//  Copyright © 2015 EL Passion. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+}
