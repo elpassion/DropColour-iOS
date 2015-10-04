b6fac6e20c4b196bdee218445866a8b2c6c73fa0
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2015-10-04 02:03:48 +0200

    Added method which show leaderboards from game center in GameOverViewController

--- ELColorGame/UI/Screens/GameOver/GameOverViewController.swift
+++ ELColorGame/UI/Screens/GameOver/GameOverViewController.swift
@@ -7,14 +7,16 @@
 //
 
 import UIKit
+import GameKit
 
-class GameOverViewController: UIViewController {
+class GameOverViewController: UIViewController, GKGameCenterControllerDelegate {
 
     var blurEffectView: UIVisualEffectView = UIVisualEffectView(frame: CGRectZero)
     let scoreTextLabel = UILabel(frame: CGRectZero)
     var scoreNumber: Int = Int()
     let cancelButton = Button(title: "CANCEL", color: UIColor(red:0.91, green:0.15, blue:0.33, alpha:1))
     let tryAgainButton = Button(title: "TRY AGAIN", color: UIColor(red:0.38, green:0.87, blue:0.1, alpha:1))
+    let topPlayersButton = Button(title: "TOP PLAYERS", color: UIColor(red:0.33, green:0.78, blue:0.78, alpha:1))
     
     init(score: Int) {
         super.init(nibName: nil, bundle: nil)
@@ -33,6 +35,7 @@ class GameOverViewController: UIViewController {
         configureScoreTextLabel()
         configureTryAgainButton()
         configureCancelButton()
+        configureTopPlayersButtons()
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -71,6 +74,25 @@ class GameOverViewController: UIViewController {
             self?.didTapOnTryAgainButton()
         }
         setupTryAgainButtonLayout()
+    }
+    
+    func configureTopPlayersButtons() {
+        topPlayersButton.buttonActionClosure = { [weak self] in
+            self?.showLeaders()
+        }
+        setupTopPlayersButtonLayout()
+    }
+    
+    //GameCenter
+    
+    func showLeaders() {
+        let gc = GKGameCenterViewController()
+        gc.gameCenterDelegate = self
+        presentViewController(gc, animated: true, completion: nil)
+    }
+    
+    func gameCenterViewControllerDidFinish(gameCenterViewController: GKGameCenterViewController) {
+        gameCenterViewController.dismissViewControllerAnimated(true, completion: nil)
     }
 
     //Action methods
@@ -119,6 +141,16 @@ class GameOverViewController: UIViewController {
         self.view.addSubview(cancelButton)
         cancelButton.snp_makeConstraints { (make) -> Void in
             make.top.equalTo(tryAgainButton.snp_bottom).offset(15)
+            make.width.equalTo(170)
+            make.height.equalTo(55)
+            make.centerX.equalTo(0)
+        }
+    }
+    
+    func setupTopPlayersButtonLayout() {
+        self.view.addSubview(topPlayersButton)
+        topPlayersButton.snp_makeConstraints { (make) -> Void in
+            make.top.equalTo(cancelButton.snp_bottom).offset(15)
             make.width.equalTo(170)
             make.height.equalTo(55)
             make.centerX.equalTo(0)
