3edd1fa1cfb7a2252536ae968d3d3e856ea1b742
Dariusz Rybicki <darrarski@gmail.com>
2015-11-27 15:18:04 +0100

    CircleType - top and bottom colors; defined types

--- ELColorGame/UI/Components/UIView/CircleType.swift
+++ ELColorGame/UI/Components/UIView/CircleType.swift
@@ -13,18 +13,25 @@ protocol _CircleType {}
 
 struct CircleType: _CircleType {
     
-    let color: UIColor
+    let topColor: UIColor
+    let bottomColor: UIColor
     
 }
 
-let CircleTypeRed = CircleType(color: UIColor.redColor())
-let CircleTypeGreen = CircleType(color: UIColor.greenColor())
-let CircleTypeBlue = CircleType(color: UIColor.blueColor())
+let CircleTypePurple = CircleType(topColor: UIColor(hex: "#C86DD7"), bottomColor: UIColor(hex: "#906DD7"))
+let CircleTypeBlue = CircleType(topColor: UIColor(hex: "#00C7FF"), bottomColor: UIColor(hex: "#0076FF"))
+let CircleTypeOrange = CircleType(topColor: UIColor(hex: "#FAD961"), bottomColor: UIColor(hex: "#F7821C"))
+let CircleTypeRed = CircleType(topColor: UIColor(hex: "#E92578"), bottomColor: UIColor(hex: "#E9253D"))
+let CircleTypeTurquoise = CircleType(topColor: UIColor(hex: "#3CFFB7"), bottomColor: UIColor(hex: "#50CFE3"))
+let CircleTypeGreen = CircleType(topColor: UIColor(hex: "#A5E01A"), bottomColor: UIColor(hex: "#32E01A"))
 
 let CircleTypes = [
+    CircleTypePurple,
+    CircleTypeBlue,
+    CircleTypeOrange,
     CircleTypeRed,
-    CircleTypeGreen,
-    CircleTypeBlue
+    CircleTypeTurquoise,
+    CircleTypeGreen
 ]
 
 extension Array where Element: _CircleType {
