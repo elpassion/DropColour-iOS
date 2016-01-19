5f88c5129aa56d2a526a228982c74fd5d1268222
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2016-01-19 16:03:04 +0100

    Start using guard instead of if

--- ELColorGame/UI/Screens/GameBoard/ViewControllers/GameViewController+GameKit.swift
+++ ELColorGame/UI/Screens/GameBoard/ViewControllers/GameViewController+GameKit.swift
@@ -6,19 +6,20 @@
 import Foundation
 import GameKit
 
+let leaderboardIdentifier = "drag_and_drop_color_leaderboard_01"
+
 extension GameViewController {
-    
+
     func synchronizeHighestScore() {
-        if GKLocalPlayer.localPlayer().authenticated {
-            let score = GKScore(leaderboardIdentifier: "drag_and_drop_color_leaderboard_01")
-            guard let game = game else { return }
-            score.value = Int64(game.scoreNumber)
-            GKScore.reportScores([score], withCompletionHandler: { (error) -> Void in
-                if error != nil {
-                    print("error")
-                }
-            })
-        }
+        guard GKLocalPlayer.localPlayer().authenticated else { return }
+        let score = GKScore(leaderboardIdentifier: leaderboardIdentifier)
+        guard let game = game else { return }
+        score.value = Int64(game.scoreNumber)
+        GKScore.reportScores([score], withCompletionHandler: { (error) -> Void in
+            if error != nil {
+                print("error")
+            }
+        })
     }
-    
+
 }
