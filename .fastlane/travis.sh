e708f21d0758214783cca80cbaffb79e4561be43
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2016-01-20 21:30:29 +0100

    Modified travis, configure with scan from fastlane and added certificates, provisioning profile

---
+++ .fastlane/travis.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+fastlane unittests
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+echo "This is a pull request. No deployment will be done."
+exit 0
+fi
+if [[ "$TRAVIS_BRANCH" != "master" ]]; then
+echo "Testing on a branch other than master. No deployment will be done."
+exit 0
+fi
+
