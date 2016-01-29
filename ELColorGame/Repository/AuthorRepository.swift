8ef4eea5747b168464eb85e933b06c629c04ee6c
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2016-01-29 20:41:38 +0100

    Added repository for authors

---
+++ ELColorGame/Repository/AuthorRepository.swift
@@ -0,0 +1,43 @@
+//
+// Created by Mateusz Szklarek on 29/01/16.
+// Copyright (c) 2016 EL Passion. All rights reserved.
+//
+
+import Foundation
+
+class AuthorRepository {
+
+    class func firstAuthor() -> Author {
+        return Author(
+            name: "Mateusz",
+            surname: "Szklarek",
+            type: AuthorType.Developer,
+            avatar: NSURL(string: "ms"),
+            proffesionUrl: NSURL(string: "https://github.com/mateuszszklarek"),
+            twitterUrl: NSURL(string: "https://twitter.com/SzklarekMateusz")
+        )
+    }
+    
+    class func secondAuthor() -> Author {
+        return Author(
+            name: "Dariusz",
+            surname: "Rybicki",
+            type: AuthorType.Developer,
+            avatar: NSURL(string: "dr"),
+            proffesionUrl: NSURL(string: "https://github.com/darrarski"),
+            twitterUrl: NSURL(string: "https://twitter.com/darrarski")
+        )
+    }
+    
+    class func thirdAuthor() -> Author {
+        return Author(
+            name: "Dawid",
+            surname: "Dapszus",
+            type: AuthorType.Designer,
+            avatar: NSURL(string: "dd"),
+            proffesionUrl: NSURL(string: "https://dribbble.com/dashoo"),
+            twitterUrl: NSURL(string: "https://twitter.com/dapszus")
+        )
+    }
+
+}
