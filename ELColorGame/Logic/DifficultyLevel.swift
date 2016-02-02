eadcff545d214259cdb540c3d6e04e4e68869134
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2016-02-02 21:16:07 +0100

    Implemented mathematical formula to function 'intervalTime'

--- ELColorGame/Logic/DifficultyLevel.swift
+++ ELColorGame/Logic/DifficultyLevel.swift
@@ -6,13 +6,19 @@
 import Foundation
 
 class DifficultyLevel {
+
+    private let initialTimeInterval: NSTimeInterval = 0.75
+    private let pointsTimeChanges: Double = 2000
+    private let initialValueForNextLevel: Double = 0.85
+    private let difficlutyRatio: Double = 2500
     
     typealias ScoreClosure = () -> Int
     
     var scoreClosure: ScoreClosure?
     
     func intervalTime() -> NSTimeInterval {
-        return 0.75 / (1 + (Double(score) / 3600))
+        let floorFromScore = floor(Double(score) / pointsTimeChanges) * pointsTimeChanges
+        return initialTimeInterval / (1 + ( (Double(score) - (floorFromScore * initialValueForNextLevel)) / difficlutyRatio ))
     }
     
     func actionPoints() -> Int {
