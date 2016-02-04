1dc080af38ef98ae831f26c1ad9b4d497c8acdc2
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2016-02-04 13:37:02 +0100

    Added spy for class which implemenet protocol GAITracker

---
+++ ELColorGameTests/Spy/GAITrackerSpy.swift
@@ -0,0 +1,29 @@
+//
+// Created by Mateusz Szklarek on 04/02/16.
+// Copyright (c) 2016 EL Passion. All rights reserved.
+//
+
+import Foundation
+
+@testable
+import ELColorGame
+
+class GAITrackerSpy: NSObject, GAITracker {
+    
+    // MARK: GAITracker
+    
+    var name = ""
+    
+    var allowIDFACollection = false
+    
+    func set(parameterName: String!, value: String!) {
+    }
+    
+    func get(parameterName: String!) -> String! {
+        return ""
+    }
+    
+    func send(parameters: [NSObject : AnyObject]!) {
+    }
+
+}
