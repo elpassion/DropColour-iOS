6d36bf0e01587084079ac0774fac89ea69590e8c
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2016-02-05 11:04:25 +0100

    Added test case track screen in MenuViewController

---
+++ ELColorGameTests/TestCase/ViewControllers/MenuViewControllerSpec.swift
@@ -0,0 +1,40 @@
+import Quick
+import Nimble
+import UIKit
+
+@testable
+import ELColorGame
+
+class MenuViewControllerSpec: QuickSpec {
+
+    override func spec() {
+
+        describe("MenuViewController") {
+
+            var sut: MenuViewController!
+            var trackerSpy: TrackerSpy!
+
+            beforeEach {
+                trackerSpy = TrackerSpy(gaiTracker: GAITrackerSpy())
+                sut = MenuViewController(delegate: nil, tracker: trackerSpy)
+            }
+
+            afterEach {
+                trackerSpy = nil
+                sut = nil
+            }
+
+            it("should be initialized") {
+                expect(sut).notTo(beNil())
+                expect(trackerSpy.screenWasTracked).to(beFalse())
+            }
+
+            describe("viewWillAppear") {
+                it("should send data") {
+                    sut.viewWillAppear(false)
+                    expect(trackerSpy.screenWasTracked).to(beTrue())
+                }
+            }
+        }
+    }
+}
