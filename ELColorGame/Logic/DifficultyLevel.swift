e9a16bee40cd4b141e651bc5965f6d55c2fdd0e1
Dariusz Rybicki <darrarski@gmail.com>
2016-02-05 13:28:23 +0100

    DifficultyLevel - interval algorithm updated

--- ELColorGame/Logic/DifficultyLevel.swift
+++ ELColorGame/Logic/DifficultyLevel.swift
@@ -8,26 +8,28 @@ import Foundation
 class DifficultyLevel {
 
     private let initialTimeInterval: NSTimeInterval = 0.75
-    private let pointsTimeChanges: Double = 2000
-    private let initialValueForNextLevel: Double = 0.85
-    private let difficlutyRatio: Double = 2500
+    private let pointsTimeChanges: Double = 200
+    private let minTimeIntervalRatio: Double = 200
+    private let maxTimeIntervalRatio: Double = 800
     
     typealias ScoreClosure = () -> Int
     
     var scoreClosure: ScoreClosure?
     
     func intervalTime() -> NSTimeInterval {
-        let floorFromScore = floor(Double(score) / pointsTimeChanges) * pointsTimeChanges
-        return initialTimeInterval / (1 + ( (Double(score) - (floorFromScore * initialValueForNextLevel)) / difficlutyRatio ))
+        let minInterval = initialTimeInterval / (1 + (score / minTimeIntervalRatio))
+        let maxInterval = initialTimeInterval / (1 + (score / maxTimeIntervalRatio))
+        let interval = maxInterval - (maxInterval - minInterval) * (score % pointsTimeChanges) / pointsTimeChanges
+        return interval
     }
     
     func actionPoints() -> Int {
         return 10
     }
     
-    private var score: Int {
+    private var score: Double {
         guard let score = scoreClosure?() else { return 0 }
-        return score
+        return Double(score)
     }
 
 }
