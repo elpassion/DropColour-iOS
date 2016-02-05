79b02627ac584258de9b04657eb2fba22392f7f8
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2016-02-05 11:01:17 +0100

    Added test case track screen in StartViewController

---
+++ ELColorGameTests/TestCase/ViewControllers/StartViewControllerSpec.swift
@@ -0,0 +1,40 @@
+import Quick
+import Nimble
+import UIKit
+
+@testable
+import ELColorGame
+
+class StartViewControllerSpec: QuickSpec {
+
+    override func spec() {
+
+        describe("StartViewController") {
+
+            var sut: StartViewController!
+            var trackerSpy: TrackerSpy!
+
+            beforeEach {
+                trackerSpy = TrackerSpy(gaiTracker: GAITrackerSpy())
+                sut = StartViewController(tracker: trackerSpy)
+            }
+
+            afterEach {
+                trackerSpy = nil
+                sut = nil
+            }
+
+            it("should be initialized") {
+                expect(sut).notTo(beNil())
+                expect(trackerSpy.screenWasTracked).to(beFalse())
+            }
+
+            describe("viewWillAppear") {
+                it("should send data") {
+                    sut.viewWillAppear(false)
+                    expect(trackerSpy.screenWasTracked).to(beTrue())
+                }
+            }
+        }
+    }
+}
