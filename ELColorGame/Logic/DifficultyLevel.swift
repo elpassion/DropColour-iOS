01a89ea41f23bdc3e757914c6c1c475ce9587236
Dariusz Rybicki <darrarski@gmail.com>
2016-02-05 15:30:23 +0100

    DifficultyLevel refactoring

--- ELColorGame/Logic/DifficultyLevel.swift
+++ ELColorGame/Logic/DifficultyLevel.swift
@@ -8,26 +8,37 @@ import Foundation
 class DifficultyLevel {
 
     private let initialTimeInterval: NSTimeInterval = 0.75
-    private let pointsTimeChanges: Double = 200
+    private let scorePerLevel: Double = 200
     private let minTimeIntervalRatio: Double = 200
     private let maxTimeIntervalRatio: Double = 800
 
     var scoreClosure: (() -> Int)?
 
     func intervalTime() -> NSTimeInterval {
-        let minInterval = initialTimeInterval / (1 + (score / minTimeIntervalRatio))
-        let maxInterval = initialTimeInterval / (1 + (score / maxTimeIntervalRatio))
-        let interval = maxInterval - (maxInterval - minInterval) * (score % pointsTimeChanges) / pointsTimeChanges
-        return interval
+        let min = minInterval(forScore: currentScore)
+        let max = maxInterval(forScore: currentScore)
+        return max - (max - min) * currentLevelProgress
     }
     
     func actionPoints() -> Int {
         return 10
     }
     
-    private var score: Double {
+    private var currentScore: Int {
         guard let score = scoreClosure?() else { return 0 }
-        return Double(score)
+        return score
+    }
+    
+    private func minInterval(forScore score: Int) -> Double {
+        return initialTimeInterval / (1 + (Double(score) / minTimeIntervalRatio))
+    }
+    
+    private func maxInterval(forScore score: Int) -> Double {
+        return initialTimeInterval / (1 + (Double(score) / maxTimeIntervalRatio))
+    }
+    
+    private var currentLevelProgress: Double {
+        return (Double(currentScore) % scorePerLevel) / scorePerLevel
     }
 
 }
