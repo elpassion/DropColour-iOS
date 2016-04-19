4ca99dc2a031c8a2fe155ea59cb3cf4087746598
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2016-04-19 10:29:07 +0200

    Revert "Remove fix for issue with SSL in Ruby (Xcode 7.2)"
    This reverts commit eb06b5a68e6b195c794a234c2de9f40bd5b334fc.

--- .fastlane/testflight.sh
+++ .fastlane/testflight.sh
@@ -17,6 +17,8 @@ if [[ "$keyword" == "[testflight]" ]]; then
 echo "Deploy to Test Flight will be done."
 # Add username to keychain
 fastlane-credentials add --username mateusz.szklarek@elpassion.com --password $FASTLANE_PASSWORD
+# Fix for issues with SSL in Ruby (only Xcode 7.2)
+rvm reinstall 2.0.0-p643 --disable-binary
 # TESTFLIGHT
 fastlane testflight
 exit 0
