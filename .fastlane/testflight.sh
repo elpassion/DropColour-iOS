da9d61cff5aa95056096af564fe983a208258229
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2016-04-28 17:37:16 +0200

    Fix for issues with SSL in Ruby (still in Xcode 7.3) :tada:

--- .fastlane/testflight.sh
+++ .fastlane/testflight.sh
@@ -17,7 +17,7 @@ if [[ "$keyword" == "[testflight]" ]]; then
 echo "Deploy to Test Flight will be done."
 # Add username to keychain
 fastlane-credentials add --username mateusz.szklarek@elpassion.com --password $FASTLANE_PASSWORD
-# Fix for issues with SSL in Ruby (only Xcode 7.2)
+# Fix for issues with SSL in Ruby (only Xcode 7.3)
 rvm reinstall 2.0.0-p643 --disable-binary
 # TESTFLIGHT
 fastlane testflight
