0f797003fe69e26bd80aa3243c8cb7bb201fd131
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2017-07-20 16:35:52 +0200

    Add tests for Array extension

---
+++ ELColorGameTests/TestCase/ArrayRandomSpec.swift
@@ -0,0 +1,34 @@
+import Quick
+import Nimble
+
+@testable import ELColorGame
+
+class ArrayRandomSpec: QuickSpec {
+
+    override func spec() {
+        describe("Random element from array") {
+            var sut: [Int]!
+
+            context("when array contained several elements") {
+                beforeEach {
+                    sut = [4, 5, 8, 10, 20]
+                }
+
+                it("should get random element contained in array") {
+                    expect(sut.contains(sut.random!)).to(beTrue())
+                }
+            }
+
+            context("when array is empty") {
+                beforeEach {
+                    sut = []
+                }
+
+                it("should get nil") {
+                    expect(sut.random).to(beNil())
+                }
+            }
+        }
+    }
+    
+}
