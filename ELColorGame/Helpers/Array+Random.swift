e747ef10661b1bbfd3ef4f3642549f687918d3f2
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2017-07-20 16:56:59 +0200

    Fix swiftlint violation in Array+Random

--- ELColorGame/Helpers/Array+Random.swift
+++ ELColorGame/Helpers/Array+Random.swift
@@ -3,7 +3,7 @@ import Foundation
 extension Array {
 
     var random: Element? {
-        guard count > 0 else { return nil }
+        guard !isEmpty else { return nil }
         return self[Int(arc4random_uniform(UInt32(count)))]
     }
 
