1099f68e506e4d327a9f85e90fc2c1cc70ffc204
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2017-07-21 12:07:51 +0200

    Fix violations in GameViewController+GameAnalytics extension

--- ELColorGame/UI/Screens/Game/ExternalServices/GameViewController+GameAnalytics.swift
+++ ELColorGame/UI/Screens/Game/ExternalServices/GameViewController+GameAnalytics.swift
@@ -4,11 +4,18 @@ import GameAnalytics
 extension GameViewController {
 
     func gameAnalyticsStartEvent() {
-        GameAnalytics.addProgressionEvent(with: GAProgressionStatusStart, progression01: "New game", progression02: nil, progression03: nil)
+        GameAnalytics.addProgressionEvent(with: GAProgressionStatusStart,
+                                          progression01: "New game",
+                                          progression02: nil,
+                                          progression03: nil)
     }
 
     func gameAnalyticsCompleteEventWithScore(_ score: Int) {
-        GameAnalytics.addProgressionEvent(with: GAProgressionStatusComplete, progression01: "Complete game", progression02: nil, progression03: nil, score: score)
+        GameAnalytics.addProgressionEvent(with: GAProgressionStatusComplete,
+                                          progression01: "Complete game",
+                                          progression02: nil,
+                                          progression03: nil,
+                                          score: score)
     }
 
 }
