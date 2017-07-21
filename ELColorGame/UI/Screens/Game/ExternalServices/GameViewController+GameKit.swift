d3335ba36f8b625f373122a2f547c0e30b785387
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2017-07-21 14:13:15 +0200

    Refactor GameViewController+GameKit extension

--- ELColorGame/UI/Screens/Game/ExternalServices/GameViewController+GameKit.swift
+++ ELColorGame/UI/Screens/Game/ExternalServices/GameViewController+GameKit.swift
@@ -7,8 +7,8 @@ extension GameViewController {
 
     func synchronizeHighestScore() {
         guard GKLocalPlayer.localPlayer().isAuthenticated else { return }
-        let score = GKScore(leaderboardIdentifier: leaderboardIdentifier)
         guard let game = game else { return }
+        let score = GKScore(leaderboardIdentifier: leaderboardIdentifier)
         score.value = Int64(game.scoreNumber)
         GKScore.report([score], withCompletionHandler: nil)
     }
